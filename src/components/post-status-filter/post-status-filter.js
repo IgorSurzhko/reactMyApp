01970facc33aa77b85cfb19a9b75eddb23119d95
Igor <ermygut@gmail.com>
2021-03-21 11:36:33 +0200

    posts

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './post-status-filter.css'
 
 const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <button/>
-            <button/>
+	return (
+		<div className="btn-group">
+			<button type='button' className='btn btn-info'>Все</button>
+			<button type='button' className='btn btn-outline-secondary'>Понравилось</button>
 
-        </div>
-    )
+		</div>
+	)
 }
 
 export default PostStatusFilter;
